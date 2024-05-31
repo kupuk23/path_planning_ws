eb230537e64e31e623d6a11ae46e328ad1b674f4
Tafarrel <kupuk23@gmail.com>
2024-05-31 21:25:43 +0000

    rapihin generate new path di test.py

--- path_planning/scripts/test.py
+++ path_planning/scripts/test.py
@@ -1,204 +1,194 @@
-import numpy as np
+#!/usr/bin/env python3
 from fsd_path_planning.utils.math_utils import unit_2d_vector_from_angle, rotate
 from fsd_path_planning.utils.cone_types import ConeTypes
 from fsd_path_planning import PathPlanner, MissionTypes, ConeTypes
-import random
-import matplotlib.pyplot as plt
-
-import random
-
-
-#TODO 
-
-GENERATE_OUTLIERS = False
-UNDETECTED_CONES = False
-FALSE_POS_CONES = False
-NUM_FALSE_POS = 2
-NUM_UNDETECTED = 2
-FIRST_TIME = True
-update_step = 5  # update the car position to the next 5 points of the path
+import rospy
+import numpy as np
+import timeit
+from std_msgs.msg import String, Int16, Float32
+from visualization_msgs.msg import MarkerArray, Marker
+from nav_msgs.msg import Odometry, Path
+from fs_msgs.msg import PlannedPath
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, Pose
+import tf
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+import threading
+# Rest of the code
 
 planner = PathPlanner(MissionTypes.trackdrive)
 
 
-def remove_random_cones(points_inner, points_outer, n):
-    # Copy the arrays to avoid modifying the original arrays
-    inner_cones = np.copy(points_inner)
-    outer_cones = np.copy(points_outer)
+class PathPlanner:
+    def __init__(self):
+        rospy.init_node("path_planner", anonymous=True)
+        rospy.Subscriber("/slam/output/odom", Odometry, self.odom_callback)
+        rospy.Subscriber("/slam/output/markers_map", MarkerArray, self.marker_callback)
+        rospy.Subscriber( "/navigation/speed_profiler/path", PlannedPath, self.curr_path_callback)
+        # self.pub = rospy.Publisher('/kthfs/result', Float32, queue_size=10)
 
-    # Randomly select n cones to remove from each array
-    inner_indices = random.sample(range(len(inner_cones)), n)
-    outer_indices = random.sample(range(len(outer_cones)), n)
+        def wrapper():
+            self.convert_path(None)
+            
+        self.original_path_pub = rospy.Publisher("/original_path", Path, queue_size=10)
+        self.generated_path_pub = rospy.Publisher("/generated_path", Path, queue_size=10)
+        self.generate_heading_pub = rospy.Publisher("/heading", Marker, queue_size=10)
+        self.yaw_test = rospy.Publisher("/yaw_test", Float32, queue_size=10)
+        rospy.Timer(rospy.Duration(1/2), self.convert_path)
+        # rospy.Timer(rospy.Duration(1/2), self.generate_new_path)
+        self.br = tf.TransformBroadcaster()
 
-    # Remove the selected cones from each array
-    inner_cones = np.delete(inner_cones, inner_indices, axis=0)
-    outer_cones = np.delete(outer_cones, outer_indices, axis=0)
+        self.curr_path = None
+        
+        self.stamp = None
+        self.cones_right_raw = np.array([])
+        self.cones_left_raw = np.array([])
+        print("running")
+        # rospy.sleep(2)
+        # self.time_check()
 
-    return inner_cones, outer_cones
+    def generate_new_path(self, car_pos, car_dir, cones_left_raw, cones_right_raw):
+        if not cones_left_raw.size:
+            return
+        
+        mask_is_left = np.ones(len(cones_left_raw), dtype=bool)
+        mask_is_right = np.ones(len(cones_right_raw), dtype=bool)
 
+        cones_left_adjusted = cones_left_raw - car_pos
+        cones_right_adjusted = cones_right_raw - car_pos
 
-def swap_cones(cones_left, cones_right, n):
-    # Get the indices of the cones to be swapped
-    swap_indices = np.random.choice(len(cones_left), n, replace=False)
+        mask_is_left[np.argsort(np.linalg.norm(cones_left_adjusted, axis=1))[5:]] = False
+        mask_is_right[np.argsort(np.linalg.norm(cones_right_adjusted, axis=1))[5:]] = False
 
-    # Swap the cones between left and right arrays
-    for index in swap_indices:
-        temp = cones_left[index].copy()
-        cones_left[index] = cones_right[index]
-        cones_right[index] = temp
+        cones_left = cones_left_raw[mask_is_left]
+        cones_right = cones_right_raw[mask_is_right]
 
-    return cones_left, cones_right
-
-
-def get_new_cones(map, car_pos, car_dir, path):
-    """
-    This function will be called when the car has updated its position and direction,
-    The new cones will be generated around the car radius.
-    """
-    pass
-
-
-def update_car_pose(path):
-    """
-    This function will update the car position and direction to the next 5 points of the path
-    """
-    path_x = path[:, 1]
-    path_y = path[:, 2]
-    new_position = np.array([path_x[update_step], path_y[update_step]])
-    dx = np.diff(
-        path_x[update_step : update_step + 3]
-    )  # get the gradient with the next 3 points
-    dy = np.diff(path_y[update_step : update_step + 3])
-    new_direction = np.array([dx[0], dy[0]])
-    return new_position, new_direction
-
-
-car_position = np.array([0.0, 0.0])
-car_direction = np.array([0.0, -1.0])
-phi_inner = np.arange(0, np.pi / 1.8, np.pi / 18)
-phi_outer = np.arange(0, np.pi / 2, np.pi / 20)
-phi_test = np.arange(0, -np.pi / 2, -np.pi / 20)
-
-rng = np.random.default_rng(5)
-
-
-outlier = np.arange(0, np.pi / 10, np.pi / 50)
-outliers = unit_2d_vector_from_angle(outlier) * 13
-points_inner = unit_2d_vector_from_angle(phi_inner) * 9  # BLUE OR LEFT CONES
-points_test = unit_2d_vector_from_angle(phi_test) * 5
-# points_inner = np.concatenate((points_inner, outliers), axis=0)
-# points_inner = np.concatenate((points_inner, outliers_random), axis=0)
-outliers = unit_2d_vector_from_angle(outlier) * 8
-points_outer = unit_2d_vector_from_angle(phi_outer) * 12
-# points_outer = np.concatenate((points_outer, outliers), axis=0)
-
-# outliers_cones_inner = np.array([[5,4], [6,5], [4,2], [4,1]])
-# outliers_cones_outer = np.array([[5,-1.3], [6,0], [7,0.5],[8,0.7],[9,1]])
-
-
-center = np.mean((points_inner[:2] + points_outer[:2]) / 2, axis=0)
-points_inner -= center
-points_outer -= center
-
-cones_left_raw = points_inner
-cones_right_raw = points_outer
-
-
-map = np.vstack((points_inner, points_outer))
-
-for step in range(7):
-
-    if not FIRST_TIME:
-        car_position, car_direction = update_car_pose(path)
-        # get_new_cones()
-
-    if UNDETECTED_CONES and FIRST_TIME:
-        cones_left_raw, cones_right_raw = remove_random_cones(
-            cones_left_raw, cones_right_raw, NUM_UNDETECTED
+        cones_unknown = np.row_stack(
+            [cones_left[~mask_is_left], cones_right[~mask_is_right]]
         )
 
-    if FALSE_POS_CONES:
-        cones_left_raw, cones_right_raw = swap_cones(
-            cones_left_raw, cones_right_raw, NUM_FALSE_POS
+        blue_color = "#7CB9E8"
+        yellow_color = "gold"
+
+        # for i, c in enumerate(ConeTypes):
+        #     print(c, f"= {i}")
+
+        cones_by_type = [np.zeros((0, 2)) for _ in range(5)]
+        cones_by_type[ConeTypes.LEFT] = cones_left
+        cones_by_type[ConeTypes.RIGHT] = cones_right
+        cones_by_type[ConeTypes.UNKNOWN] = cones_unknown
+
+        out = planner.calculate_path_in_global_frame(
+            cones_by_type, car_pos, car_dir, return_intermediate_results=True
         )
 
-    if GENERATE_OUTLIERS and FIRST_TIME:
-        cones_left_flipped = cones_right_raw[:].copy()
-        cones_left_flipped[:, 1] += 10
-        cones_left_flipped[:, 0] += 10
+        (
+            path,
+            sorted_left,
+            sorted_right,
+            left_cones_with_virtual,
+            right_cones_with_virtual,
+            left_to_right_match,
+            right_to_left_match,
+        ) = out
 
-        cones_right_flipped = cones_left_raw[:].copy()
-        cones_right_flipped[:, 1] += 7
-        cones_right_flipped[:, 0] += 7
-        cones_left_raw = np.concatenate((cones_left_raw, cones_right_flipped))
-        cones_right_raw = np.concatenate((cones_right_raw, cones_left_flipped))
+        generated_path = Path()
+        generated_path.header.frame_id = "odom"
+        generated_path.header.stamp = self.stamp
+        for x,y in zip(path[1], path[2]):
+            poseStamp = PoseStamped()
+            poseStamp.header.frame_id = "odom"
+            poseStamp.header.stamp = self.stamp
+            poseStamp.pose.position.x = x
+            poseStamp.pose.position.y = y
+            generated_path.poses.append(poseStamp)
+        self.generated_path_pub.publish(generated_path)
 
-    rng = np.random.default_rng()
-    rng.shuffle(cones_left_raw)
-    rng.shuffle(cones_right_raw)
 
-    mask_is_left = np.ones(len(cones_left_raw), dtype=bool)
-    mask_is_right = np.ones(len(cones_right_raw), dtype=bool)
+    def time_check(self):
+        def wrapper():
+            self.convert_path(None)
+        func = wrapper
+        t = timeit.Timer(func)
+        execution_time = t.timeit(number=100)
+        print(f"Execution time: {execution_time/100} seconds")
 
-    cones_left = (
-        []
-    )  # for demonstration purposes, we will only keep the color of the first 4 cones
-    cones_right = []
+    def convert_path(self, event):
+        converted_path = Path()
+        if self.curr_path is not None and self.markers is not None:
+            converted_path.header.frame_id = "odom"
+            converted_path.header.stamp = self.stamp
+            for x,y in zip(self.curr_path.x, self.curr_path.y):
+                poseStamp = PoseStamped()
+                poseStamp.header.frame_id = "odom"
+                poseStamp.header.stamp = self.stamp
+                poseStamp.pose.position.x = x
+                poseStamp.pose.position.y = y
+                converted_path.poses.append(poseStamp)
+            self.original_path_pub.publish(converted_path)
 
-    cones_left_adjusted = cones_left_raw - car_position
-    cones_right_adjusted = cones_right_raw - car_position
 
-    mask_is_left[np.argsort(np.linalg.norm(cones_left_adjusted, axis=1))[5:]] = False
-    mask_is_right[np.argsort(np.linalg.norm(cones_right_adjusted, axis=1))[5:]] = False
 
-    cones_left = cones_left_raw[mask_is_left]
-    cones_right = cones_right_raw[mask_is_right]
-    cones_unknown = np.row_stack(
-        [cones_left_raw[~mask_is_left], cones_right_raw[~mask_is_right]]
-    )
+    def curr_path_callback(self, curr_path):
+        self.curr_path = curr_path
+        # rospy.loginfo(self.curr_path)
 
-    blue_color = "#7CB9E8"
-    yellow_color = "gold"
+    def marker_callback(self, markers):
+        self.markers = markers
+        marker_array = MarkerArray()
 
-    for i, c in enumerate(ConeTypes):
-        print(c, f"= {i}")
+        self.cones_right_raw = np.array([])
+        self.cones_left_raw = np.array([])
+        for i,marker in enumerate(self.markers.markers):
+            position = np.array([marker.pose.position.x, marker.pose.position.y])
+            if marker.color.b == 1.0:
+                if self.cones_right_raw.size == 0:  # Initialize if empty
+                    self.cones_right_raw = position
+                else:
+                    self.cones_right_raw = np.vstack((self.cones_right_raw, position))
+            else:
+                if self.cones_left_raw.size == 0:  # Initialize if empty
+                    self.cones_left_raw = position
+                else:
+                    self.cones_left_raw = np.vstack((self.cones_left_raw, position))
 
-    cones_by_type = [np.zeros((0, 2)) for _ in range(5)]
-    cones_by_type[ConeTypes.LEFT] = cones_left
-    cones_by_type[ConeTypes.RIGHT] = cones_right
-    cones_by_type[ConeTypes.UNKNOWN] = cones_unknown
+        
 
-    out = planner.calculate_path_in_global_frame(
-        cones_by_type, car_position, car_direction, return_intermediate_results=True
-    )
+    def odom_callback(self, odom):
+        
+        self.stamp = odom.header.stamp
+        self.car_x = odom.pose.pose.position.x
+        self.car_y = odom.pose.pose.position.y
+        quart_angle = [odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w]
+        self.car_yaw = euler_from_quaternion(quart_angle)[2]
+        self.generate_marker(odom)
+        self.car_position = np.array([self.car_x, self.car_y])
+        self.car_direction = unit_2d_vector_from_angle(self.car_yaw)
+        self.generate_new_path(self.car_position, self.car_direction, self.cones_left_raw, self.cones_right_raw)
+        
+    def generate_marker(self, odom):
+        heading = Marker()
+        heading.header.frame_id = "odom"
+        heading.header.stamp = odom.header.stamp
+        heading.type = 0
+        heading.id = 100
+        heading.scale.x = 0.7
+        heading.scale.y = 0.15
+        heading.scale.z = 0.15
+        heading.pose.position.x = self.car_x
+        heading.pose.position.y = self.car_y
+        heading.pose.position.z = 0
+        heading.pose.orientation.x = odom.pose.pose.orientation.x
+        heading.pose.orientation.y = odom.pose.pose.orientation.y
+        heading.pose.orientation.z = odom.pose.pose.orientation.z
+        heading.pose.orientation.w = odom.pose.pose.orientation.w
+        heading.color.a = 1.0
+        heading.color.r = 1.0
 
-    (
-        path,
-        sorted_left,
-        sorted_right,
-        left_cones_with_virtual,
-        right_cones_with_virtual,
-        left_to_right_match,
-        right_to_left_match,
-    ) = out
+        self.generate_heading_pub.publish(heading)
 
-    plt.clf()
-    plt.scatter(cones_left[:, 0], cones_left[:, 1], c=blue_color, label="left")
-    plt.scatter(cones_right[:, 0], cones_right[:, 1], c=yellow_color, label="right")
-    plt.scatter(cones_unknown[:, 0], cones_unknown[:, 1], c="k", label="unknown")
-
-    plt.legend()
-
-    plt.plot(
-        [car_position[0], car_position[0] + car_direction[0]],
-        [car_position[1], car_position[1] + car_direction[1]],
-        c="k",
-    )
-    plt.title("Computed path")
-    plt.plot(*path[:, 1:3].T)  # take path[1] and path[2] to get the x y of the paths
-
-    plt.axis("equal")
-
-    plt.show()
-
-    FIRST_TIME = False
+if __name__ == "__main__":    
+    try:
+        node = PathPlanner()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
